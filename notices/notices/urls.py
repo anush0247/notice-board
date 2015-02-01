d02ab53a553762ab61880c8c5bb48f905e9a7425
anush0247 <anush0247@gmail.com>
2015-02-01 13:00:59 +0530

    notice detail view created

--- notices/notices/urls.py
+++ notices/notices/urls.py
@@ -4,14 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 from .views import NoticesView
+from .views import OneNoticeView
 from .views import HomePageView
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', auth(HomePageView), name="home"),
-                       url(r'^notices/$', auth(NoticesView.as_view()), name="notices"),
-                       url(r"^login/$", "django.contrib.auth.views.login",
-                           {"template_name": "login2.html"}, name="login"),
-                       url(r"^logout/$", "django.contrib.auth.views.logout_then_login",
-                           name="logout"),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', auth(HomePageView), name="home"),
+    url(r'^notices/$', auth(NoticesView.as_view()), name="notices"),
+    url(r"^notices/(?P<pk>\d+)$", auth(OneNoticeView.as_view()), name="notice_detail"),
+    url(r"^login/$", "django.contrib.auth.views.login", {"template_name": "login.html"}, name="login"),
+    url(r"^logout/$", "django.contrib.auth.views.logout_then_login", name="logout"),
 )
--- notices/notices/views.py
+++ notices/notices/views.py
@@ -1,16 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required as auth
 from .models import Notices
 
 
 def HomePageView(request):
     return render(request, "home.html")
-
+    
 class NoticesView(ListView):
     model = Notices
     paginate_by = 5
 
+class OneNoticeView(DetailView):
+    model = Notices
 
 
 
