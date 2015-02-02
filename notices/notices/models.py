3ae626d813bbbd7e730b08700949e75314749fb2
anush0247 <anush0247@gmail.com>
2015-02-03 03:06:32 +0530

    compilation

--- notices/notices/models.py
+++ notices/notices/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Notices(models.Model):
-    receiver = (
+    receiver_labels = (
         ("E4", "Final Year Students"),
         ("E3", "Third Year Students"),
         ("E2", "Second Year Students"),
@@ -13,7 +13,7 @@ class Notices(models.Model):
     sender = models.ForeignKey(User)
     sentat = models.DateTimeField(auto_now_add=True)
     body = models.TextField(blank=True)
-    receiver = models.CharField(max_length=3,choices=receiver)
+    receiver = models.CharField(max_length=3,choices=receiver_labels)
     attachment = models.FileField(upload_to='notice_attachments/', blank=True, null=True)
     
     def __unicode__(self):
