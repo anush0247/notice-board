212e1ffca5415508b21f67f9763cbb9c9971c812
anush0247 <anush0247@gmail.com>
2015-04-06 14:36:17 +0530

    settings -> change password completed

--- rid/auth/urls.py
+++ rid/auth/urls.py
@@ -1,9 +1,12 @@
 # importing django url patterns modules
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required as auth
+from .views import PasswordChangeView
 
 # auth url patterns
 urlpatterns = patterns('',
     url(r"^login/$", "django.contrib.auth.views.login", {"template_name": "auth/login.html"}, name="login"),
     url(r"^logout/$", "django.contrib.auth.views.logout_then_login", name="logout"),
+    url(r'^settings/change_password/$',auth(PasswordChangeView.as_view()) , name="change_password"),
 )
  
--- rid/auth/views.py
+++ rid/auth/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+
+class PasswordChangeView(FormView):
+    template_name = 'auth/settings/change_password.html'
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('change_password')
+
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Your password has been changed.")
+        return super(FormView, self).form_valid(form)
