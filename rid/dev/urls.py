9aa3c216e1d53433bdd12826ebdd1cf76df26b44
anush0247 <anush0247@gmail.com>
2015-04-14 11:08:18 +0530

    some ui fixes

--- rid/dev/urls.py
+++ rid/dev/urls.py
@@ -1,10 +1,10 @@
 # importing django url patterns modules
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
-from .views import DevIntro
+from .views import DevDoc
 
 # auth url patterns
 urlpatterns = patterns('',
-    url(r"^intro/$", DevIntro, name="dev_into"),
+    url(r"^docs/$", DevDoc, name="dev_doc"),
 )
  
--- rid/dev/views.py
+++ rid/dev/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required as auth
 
 @auth
-def DevIntro(request):
-        return render(request, "dev/intro.html")
+def DevDoc(request):
+        return render(request, "dev/docs.html")
 
